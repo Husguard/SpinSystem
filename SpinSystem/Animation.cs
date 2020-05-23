53ec692af1505e8f463fc0849f9306892df37c30
Husguard <max-324@mail.ru>
2020-05-23 19:29:54 +1000

    Start spins

--- SpinSystem/Animation.cs
+++ SpinSystem/Animation.cs
@@ -12,10 +12,21 @@ namespace SpinSystem
         public int Width { get; set; }
         public int Height { get; set; }
         public List<List<List<byte>>> Data { get; set; }
+        FileStream re;
+        BufferedStream stream;
+        public Animation()
+        {
+            re = File.Create("C:/Users/Админ/Desktop/file1.json");
+            stream = new BufferedStream(re, 24000);
+        }
+        ~Animation()
+        {
+            stream.Dispose();
+            re.Dispose();
+        }
         public void ExportToData(Spin[,] spins)
         {
             this.Data.Add(new List<List<byte>>());
-            // i - номер текущего добавленного кадра
             foreach (Spin obj in spins)
             {
                 if (obj.GetSpin() == 1) this.Data[this.Data.Count - 1].Add(new List<byte> { 255, 255, 255 });
@@ -24,11 +35,7 @@ namespace SpinSystem
         }
         public void ExportToFile()
         {
-            using (FileStream re = File.Create("C:/Users/Админ/Desktop/file1.json"))
-            {
-                using (BufferedStream stream = new BufferedStream(re, 24000))
-                    JsonSerializer.SerializeToStream(this, stream);
-            }
+            JsonSerializer.SerializeToStream(this, stream);
         }
     }
 }
